0c987b63162964e4709b7e6b84919950ecefc9e1
BuildTools <unconfigured@null.spigotmc.org>
2019-03-09 17:14:05 +1100

    Changed to GTKMM and glade

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <gtkmm.h>
+#include <iostream>
+
+Gtk::Window* window;
 
 int main(int argc, char *argv[])
 {
-  auto app =
-    Gtk::Application::create(argc, argv,
-      "org.gtkmm.examples.base");
+    auto app = Gtk::Application::create(argc, argv, "com.wizzledonker.mtrack");
+	  
+	// Load the main GLADE ui
+	auto builder = Gtk::Builder::create();
 
-  Gtk::Window window;
-  window.set_default_size(1280, 720);
+	try {
+		builder->add_from_file("res/main_window.glade");
+	}
+	catch(const Glib::FileError& ex) {
+		std::cerr << "FileError: " << ex.what() << std::endl;
+		return 1;
+	}
 
-  return app->run(window);
+	builder->get_widget("main_window", window);
+
+	return app->run(*window);
 }
